213bc1945f471f267777aaa3a858c59b54c48da4
kcselm <kcselm93@gmail.com>
2019-12-17 20:19:40 -0600

    Binary search solution

---
+++ searches/binary_search.py
@@ -0,0 +1,23 @@
+# O(log(n)) time   | O(1) space 
+array = [0, 1, 21, 33, 45, 61, 71, 72, 73, 355]
+target = 355
+
+def binary_search(array, target):
+    return binary_search_helper(array, target, 0, len(array) - 1)
+
+def binary_search_helper(array, target, left, right):
+    while left <= right:
+        middle = (left + right) // 2 
+        potentialMatch = array[middle]
+        if target == potentialMatch:
+            return middle
+        elif target < potentialMatch: 
+            right = middle -1
+            # return binary_search_helper(array, target, left, middle - 1)
+        else:
+            left = middle + 1
+            # return binary_search_helper(array, target, middle + 1, right)
+    return -1
+
+
+print(binary_search(array, target))
