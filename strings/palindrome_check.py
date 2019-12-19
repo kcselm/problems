34d0635dea60d0fe2f97196550fc5972d6307c8a
kcselm <kcselm93@gmail.com>
2019-12-18 20:56:39 -0600

    Palindrome check solution

---
+++ strings/palindrome_check.py
@@ -0,0 +1,15 @@
+# O(n) time | O(1) space 
+
+string = "tacocat"
+
+def is_palindrome(string):
+    left = 0
+    right = len(string) - 1
+    while left < right:
+        if string[left] != string[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+print(is_palindrome(string))
